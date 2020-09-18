91292929985502039e9ac3f384a9b2a3b6394fbc
grzegab <greg@grzegab.eu>
2020-09-18 07:37:25 +0200

    feat(develop): create basic separate start page

--- src/UI/Rest/Landing.php
+++ src/UI/Rest/Landing.php
@@ -5,15 +5,31 @@ declare(strict_types=1);
 namespace App\UI\Rest;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class Landing
 {
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
-     * Starting point for APP.
+     * Starting point for APP. Show search and options.
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function index(): Response
     {
-        return new Response('ok');
+        $html = $this->twig->render('Landing/start.html.twig');
+
+        return new Response($html);
     }
 }
